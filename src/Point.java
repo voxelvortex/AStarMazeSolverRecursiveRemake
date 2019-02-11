6316b5cc9c93091bab39bf6e87ebfc911072bc23
Michael <maestromike5@gmail.com>
2019-02-11 18:44:56 -0500

    Added basic versions of everything, and stole the MazeMaker class from the original AStarMazeSolver

---
+++ src/Point.java
@@ -0,0 +1,15 @@
+public class Point {
+    private int x;
+    private int y;
+    private int value;
+
+    Point(int x, int y, int value){
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+
+    int getX(){return x;}
+
+    int getY(){return y;}
+}
