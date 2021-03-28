165371aa0029169a4d0bbe4779bc14b068e685dc
Michael <maestromike5@gmail.com>
2021-03-28 03:15:52 -0400

    wrote out Location class

---
+++ src/Location.java
@@ -0,0 +1,23 @@
+public class Location {
+    private int row;
+    private int col;
+    private boolean isWall;
+    
+    public Location(int row, int col){this(row,col,false);}
+    public Location(int row, int col, boolean isWall){
+        this.row = row;
+        this.col = col;
+        this.isWall = isWall;
+    }
+
+    public boolean isWall(){return isWall;}
+    public int getRow(){return row;}
+    public int getCol(){return col;}
+
+    public void setRow(int row){
+        this.row = row;
+    }
+    public void setCol(int col){
+        this.col = col;
+    }
+}
