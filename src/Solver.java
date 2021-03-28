1c119fa7f9399359e9026501ff1f9768f93de725
Michael <maestromike5@gmail.com>
2021-03-28 03:01:59 -0400

    starting over from scratch

--- src/Solver.java
+++
@@ -1,9 +0,0 @@
-public class Solver {
-    public static void AStarSolve(int[][] a){
-        for(int row = 0; row < a.length; row++){
-            for(int col = 0; col < a[row].length; col++){
-
-            }
-        }
-    }
-}
