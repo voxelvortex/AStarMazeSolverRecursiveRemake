91366e1dd7fd170657d84b9d36afc0406dd4a5e1
Michael <maestromike5@gmail.com>
2021-03-28 04:35:36 -0400

    wrote out basic solver class

---
+++ src/Solver.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Solver {
+    private Maze maze;
+    private Location start;
+    private Location end;
+
+    public Solver(Maze maze, Location start, Location end){
+        this.maze = maze;
+        this.start = start;
+        this.end = end;
+    }
+
+    public ArrayList<WNode> solve(){
+        ArrayList<WNode> starterPath = new ArrayList<>();
+        ArrayList<MazeConfig> configs = new ArrayList<>();
+        HashSet<WNode> checked = new HashSet<>();
+        
+        starterPath.add( maze.getNode(start.getRow(), start.getCol()) );
+        configs.add( new MazeConfig(starterPath) );
+
+        while(!configs.isEmpty()){
+            MazeConfig current = configs.remove(0);
+            for(MazeConfig potentialSolution: current.getSuccessors(maze, checked)){
+
+                if(potentialSolution.isGoal(end)){
+                    return potentialSolution.getPath();
+                }
+
+                if(potentialSolution.isValid()){
+                    configs.add(potentialSolution);
+                }
+            }
+
+            Collections.sort(configs);
+        }
+
+        return null;
+    }
+
+}
